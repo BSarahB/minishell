e5fbbb0c1b3903b57484b485e395ad53a74321e0
Mehdia BENMESBAH <mbenmesb@e2r3p5.clusters.42paris.fr>
2023-02-18 19:10:04 +0100

    adding CMake configs

---
+++ build/CMakeFiles/my_minishell.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/nfs/homes/mbenmesb/Documents/my_minishell/srcs/executor.c" "CMakeFiles/my_minishell.dir/srcs/executor.c.o" "gcc" "CMakeFiles/my_minishell.dir/srcs/executor.c.o.d"
+  "/mnt/nfs/homes/mbenmesb/Documents/my_minishell/srcs/ft_struct_init.c" "CMakeFiles/my_minishell.dir/srcs/ft_struct_init.c.o" "gcc" "CMakeFiles/my_minishell.dir/srcs/ft_struct_init.c.o.d"
+  "/mnt/nfs/homes/mbenmesb/Documents/my_minishell/srcs/get_path.c" "CMakeFiles/my_minishell.dir/srcs/get_path.c.o" "gcc" "CMakeFiles/my_minishell.dir/srcs/get_path.c.o.d"
+  "/mnt/nfs/homes/mbenmesb/Documents/my_minishell/srcs/main.c" "CMakeFiles/my_minishell.dir/srcs/main.c.o" "gcc" "CMakeFiles/my_minishell.dir/srcs/main.c.o.d"
+  "/mnt/nfs/homes/mbenmesb/Documents/my_minishell/srcs/setting_redirections.c" "CMakeFiles/my_minishell.dir/srcs/setting_redirections.c.o" "gcc" "CMakeFiles/my_minishell.dir/srcs/setting_redirections.c.o.d"
+  "/mnt/nfs/homes/mbenmesb/Documents/my_minishell/srcs/shell_lexer.c" "CMakeFiles/my_minishell.dir/srcs/shell_lexer.c.o" "gcc" "CMakeFiles/my_minishell.dir/srcs/shell_lexer.c.o.d"
+  "/mnt/nfs/homes/mbenmesb/Documents/my_minishell/srcs/shell_parser.c" "CMakeFiles/my_minishell.dir/srcs/shell_parser.c.o" "gcc" "CMakeFiles/my_minishell.dir/srcs/shell_parser.c.o.d"
+  "/mnt/nfs/homes/mbenmesb/Documents/my_minishell/srcs/update_free_memory.c" "CMakeFiles/my_minishell.dir/srcs/update_free_memory.c.o" "gcc" "CMakeFiles/my_minishell.dir/srcs/update_free_memory.c.o.d"
+  "/mnt/nfs/homes/mbenmesb/Documents/my_minishell/srcs/utils.c" "CMakeFiles/my_minishell.dir/srcs/utils.c.o" "gcc" "CMakeFiles/my_minishell.dir/srcs/utils.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/my_minishell.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_minishell.dir/srcs/executor.c.o"
+  "CMakeFiles/my_minishell.dir/srcs/executor.c.o.d"
+  "CMakeFiles/my_minishell.dir/srcs/ft_struct_init.c.o"
+  "CMakeFiles/my_minishell.dir/srcs/ft_struct_init.c.o.d"
+  "CMakeFiles/my_minishell.dir/srcs/get_path.c.o"
+  "CMakeFiles/my_minishell.dir/srcs/get_path.c.o.d"
+  "CMakeFiles/my_minishell.dir/srcs/main.c.o"
+  "CMakeFiles/my_minishell.dir/srcs/main.c.o.d"
+  "CMakeFiles/my_minishell.dir/srcs/setting_redirections.c.o"
+  "CMakeFiles/my_minishell.dir/srcs/setting_redirections.c.o.d"
+  "CMakeFiles/my_minishell.dir/srcs/shell_lexer.c.o"
+  "CMakeFiles/my_minishell.dir/srcs/shell_lexer.c.o.d"
+  "CMakeFiles/my_minishell.dir/srcs/shell_parser.c.o"
+  "CMakeFiles/my_minishell.dir/srcs/shell_parser.c.o.d"
+  "CMakeFiles/my_minishell.dir/srcs/update_free_memory.c.o"
+  "CMakeFiles/my_minishell.dir/srcs/update_free_memory.c.o.d"
+  "CMakeFiles/my_minishell.dir/srcs/utils.c.o"
+  "CMakeFiles/my_minishell.dir/srcs/utils.c.o.d"
+  "my_minishell"
+  "my_minishell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_minishell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
