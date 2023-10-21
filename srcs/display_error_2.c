0877d85b3bf7464179751ef289b2fff83944331a
Mehdia BENMESBAH <mbenmesb@bess-f2r6s5.clusters.42paris.fr>
2023-10-21 20:03:07 +0200

    ’param’

--- srcs/display_error_2.c
+++ srcs/display_error_2.c
@@ -21,9 +21,9 @@ void	ft_error(char *const str)
 void	ft_error_msg6(char *infile)
 {
 
-	ft_error("env: '");
+	ft_error("env: ’");
 	ft_error(infile);//ON DOIT REMPLACER ARGV[1] par le nom d INFILE (normalement cela est la 1 ere simple_cmd) (verifier l impact d un infile dans un block)
-	ft_error("': No such file or directory");
+	ft_error("’: No such file or directory");
 	ft_error("\n");
 }
 
